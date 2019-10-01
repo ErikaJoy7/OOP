eee1c15e54a52756182656f2a065939bdb180443
ErikaJoy7 <55729989+ErikaJoy7@users.noreply.github.com>
2019-10-01 20:37:00 +0800

    Update geometricLab.java

--- geometricLab.java
+++ geometricLab.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 
 public class geometricLab {
         
-    //Scanner input = new Scanner(System.in);
     
+    //main method
     public static void main (String[] args){
       
       
@@ -22,8 +22,7 @@ public class geometricLab {
        Box objj = new Box(2, 3, 4);
 
         
-        //System.out.println("Box a: " + areaa.toString());
-        //areaa.rectangleFormula();
+  
         System.out.println("Box a: " + objj.toString());
         System.out.println("Box a surface area: " + objj.surfacearea);
         System.out.println("Box b: " + areaa.toString());
@@ -47,7 +46,7 @@ public class geometricLab {
     }    
 
 
-
+//start of Rectangle2 class
 public static class Rectangle2{
     public int base,l=4,w=7,h=4;
     int area = ((l*w)+(l*h)+(w*h))*2;
@@ -68,14 +67,14 @@ public static class Rectangle2{
       public String toString(){
         return "[length="+ l + " " + ", width="+ " " + w + ", height="+ " " + h + "]";
         }
-    }
+    }//end of Rectangle2 class
     
 
 
-
+       // class Box
     public static class Box extends Rectangle2{
     
-    //Scanner input = new Scanner(System.in);
+   
     private int h=4;
     public int width=3, length=2;
     public int surfacearea=((length*width)+(length*h)+(width*h))*2; 
@@ -88,11 +87,10 @@ public static class Rectangle2{
     }
     
     public void rectangleFormula(){
-       //area = [(length*width)+(length*h)+(width*h)*2] ;
+      
     }
     public int getArea(){
-        //System.out.println("Box a surface area: ");
-        //this.area = input.nextInt();
+        
         return surfacearea;
     }
     
@@ -115,8 +113,10 @@ public static class Rectangle2{
     }
     
    
-}
+}//end of class Box
 
+        
+//start of Triangle2 class
 public static class Triangle2{
     
     double equalSide = 3, area, sideC;
@@ -139,8 +139,9 @@ public static class Triangle2{
     }
 
    
-}
+}//end of Triangle2 class
     
+        //IsoscelesRight class
     public static class IsoscelesRight extends Triangle2{
  
         public IsoscelesRight(doubl­e equalSize){
